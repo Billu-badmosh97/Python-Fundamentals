ff20250e311f2e8cb2ecd8a92bc5b6f688f20c56
Billu-badmosh97 <studyworld076@gmail.com>
2026-06-21 21:26:12 +0530

    Day-1_python_basics.py

---
+++ Day-1_python_basics.py
@@ -0,0 +1,57 @@
+print("hello world", 45)
+print(56)
+name = "Billu Badmoshh"
+Age = 19
+Life = 99.99
+total_HP = Life
+Current_Hp = Life-Age
+
+print ("the Name is:", name)
+print("The Age is:", Age)
+print("Lifeline Left:", Current_Hp)
+print("total HP:", total_HP)
+print("This is a Comment")
+print(Life%Age)
+print(Life**Age)
+
+#logical Operators
+print(not False)
+a = 30 
+b = 76
+print(not a > b)
+val1 = True
+Val2 = False
+print("ans:", val1 and Val2)
+print(a==b and a<b)
+print(a==b or a<b)
+
+#inputs
+name = input("Your name?")
+print("Ou Welcome", name, "my bad Nice To Meet Ya")
+Age = int(input("How old are you?"))
+print("Your Age is", Age, "nice")
+print("Practice Sum of two Numbers")
+Num1 = int(input("First No."))
+Num2 = int(input("second No."))
+print("sum=", Num1 + Num2)
+#Practive Area of a rectangle
+
+Sid1 = int(input("Your Length is:"))
+Sid2 = int(input("Breadth"))
+print("Area = ", Sid1 * Sid2)
+
+#area of square
+print("lets find Area of Square")
+s = float(input("Your Length is:"))
+print("area = ", s*s)
+#Average
+print("lets find Avg for 2 nos.")
+num1 = float(input("1st No."))
+num2 = float(input("2nd No."))
+print("Average = ", (num1+num2)/2)
+
+#Boolean Practice
+No1 = int(input("a="))
+No2 = int(input("b="))
+print("a>b?")
+print(No1>=No2)
